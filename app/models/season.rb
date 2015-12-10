038b867a8a889356e1cb32d40e54e5b57041505f
Jeremy Sklarsky <jsklarsky@mediamath.com>
2015-12-09 21:46:38 -0500

    include statable on seasons

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,11 +2,5 @@ class Season < ActiveRecord::Base
   belongs_to :show
   has_many :episodes
 
-  def total_points
-    self.episodes.collect {|episode| episode.rating}.inject(:+)
-  end
-
-  def average 
-    (total_points / self.episodes.size.to_f).round(2)
-  end
+  include Statable::InstanceMethods
 end
